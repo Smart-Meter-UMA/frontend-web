9b748c566957fabb82c5c27ad0b0c8c98287cb80
Antonio Santiago Diaz <antoniosd@uma.es>
2022-05-13 19:01:20 +0200

    Vista hogar y dispositivos

--- src/web/hogar.js
+++ src/web/hogar.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import { Button, ButtonGroup, Col, Container, Row, ToggleButton } from "react-bootstrap";
+import { Button, ButtonGroup, Col, Container, Row, Tabs, ToggleButton } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import Loading from "../components/Loading";
 import Modal from 'react-bootstrap/Modal'
+import Tab from 'react-bootstrap/Tab'
+import AnyChart from 'anychart-react'
 
 function Hogar(){
     let {id} = useParams();
@@ -14,6 +16,8 @@ function Hogar(){
 
     const [compartidos, setCompartidos] = useState([]);
     const [hayCompartidos, setHayCompartidos] = useState(false)
+
+    const [key, setKey] = useState('filtrar');
 
     const [show, setShow] = useState(false);
 
@@ -95,6 +99,8 @@ function Hogar(){
                 </Row>
             </Container>
                 <br/>
+            <Row>
+                <Col>
                 {!hayDispositivos &&
                 <h3>No hay dispositivos</h3>
                 }
@@ -115,6 +121,159 @@ function Hogar(){
                 ))}
                 </ButtonGroup> 
                 }
+                </Col>
+            </Row>
+            <br/>
+            <Row>
+                <Col></Col>
+                <Col>
+                    Consumido de hoy: 
+                </Col>
+                <Col>
+                    Consumido de este mes:
+                </Col>
+                <Col></Col>
+            </Row>
+            <Row>
+            <Col></Col>
+                <Col></Col>
+                <Col>
+                    10 KW/h
+                </Col>
+                <Col>
+                </Col>
+                <Col>
+                    200 KW/h
+                </Col>
+                <Col></Col>
+                <Col></Col>
+            </Row>
+            <Row>
+                <Col></Col>
+                <Col><Row>Medida diaria:</Row></Col>
+                <Col>
+                    10 KW/h
+                </Col>
+                <Col><Row>Medida mensual:</Row></Col>
+                <Col>
+                    200 KW/h
+                </Col>
+                <Col></Col>
+                <Col></Col>
+            </Row>
+            <Row>
+            <Col></Col>
+                <Col><Row>Limite minimo diario:</Row></Col>
+                <Col>
+                    10 KW/h
+                </Col>
+                <Col>
+                   <Row>Limite minimo mensual:</Row>
+                </Col>
+                <Col>
+                    200 KW/h
+                </Col>
+                <Col></Col>
+                <Col></Col>
+            </Row>
+            <Row>
+            <Col></Col>
+                <Col><Row>Limite máximo diario:</Row></Col>
+                <Col>
+                    10 KW/h
+                </Col>
+                <Col>
+                   <Row>Limite máximo mensual:</Row>
+                </Col>
+                <Col>
+                    200 KW/h
+                </Col>
+                <Col></Col>
+                <Col></Col>
+            </Row>
+            <Row>
+                <Col sm={2}></Col>
+                <Col>
+                    <Row>
+                        <Tabs
+                            id="controlled-tab-example"
+                            activeKey={key}
+                            onSelect={(k) => setKey(k)}
+                            className="mb-3"
+                        >
+                            <Tab eventKey="filtrar" title="Filtrar">
+                                <Container>
+                                    <Row>
+                                        <Col><Row>Filtrar por:</Row></Col>
+                                        <Col sm={2}><Row><label><input type="checkbox"/>Tramos de tiempo</label></Row></Col>
+                                        <Col sm={4}><Row><label><input type="checkbox"/>Valores máximos y mínimos de consumo</label></Row></Col>
+                                        <Col></Col>
+                                        <Col></Col>
+                                        <Col></Col>
+                                        <Col></Col>
+                                    </Row>
+                                    <Row>
+                                        <Col sm={4}>
+                                            <Row>
+                                                <Col><Row>Fecha de inicio:</Row></Col>
+                                                <Col><Row><input type={"datetime-local"}/></Row></Col>
+                                            </Row>    
+                                        </Col>
+                                        <Col></Col>
+                                        <Col sm={7}>
+                                            <Row>
+                                                <Col><Row>Fecha fin:</Row></Col>
+                                                <Col><Row><input type={"datetime-local"}/></Row></Col>
+                                                <Col></Col>
+                                                <Col></Col>
+                                                <Col></Col>
+                                                <Col></Col>
+                                            </Row>    
+                                        </Col>
+                                    </Row>
+                                    <br/>
+                                    <Row>
+                                        <Col sm={4}>
+                                            <Row>
+                                                <Col><Row>Valor mínimo:</Row></Col>
+                                                <Col><Row><input type={"number"}/></Row></Col>
+                                            </Row>    
+                                        </Col>
+                                        <Col></Col>
+                                        <Col sm={7}>
+                                            <Row>
+                                                <Col><Row>Valor máximo:</Row></Col>
+                                                <Col><Row><input type={"number"}/></Row></Col>
+                                                <Col></Col>
+                                                <Col></Col>
+                                                <Col></Col>
+                                            </Row>    
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <AnyChart
+                                            id="lineChart"
+                                            width={800}
+                                            height={600}
+                                            type="line"
+                                            data={[[2,3],[3,5]]}
+                                            title="KW/H"
+                                        />
+                                    </Row>
+                                </Container>
+                            </Tab>
+                            <Tab eventKey="estadisticas" title="Estadisticas">
+                                Profile
+                            </Tab>
+                            <Tab eventKey="predicciones" title="Prediciones">
+                                Contact
+                            </Tab>
+                        </Tabs>
+
+                    </Row>
+                </Col>
+                <Col sm={2}></Col>
+            </Row>
                 
                 
             </>
